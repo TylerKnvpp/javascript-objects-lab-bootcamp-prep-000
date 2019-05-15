6818ef8fabaa65d21bf10134fc28c6231e83fe30
tyler knapp <tgknapp11@gmail.com>
2019-05-15 17:00:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
   var newObj = object.assign({}, object);
   delete newObj[key];
-  
+  return newObj
 }
