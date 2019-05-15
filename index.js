db85048ef6fab71cc411eee4c2dff9e6285f8352
tyler knapp <tgknapp11@gmail.com>
2019-05-15 17:01:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-var recipes = {};
+var recipes = {
 
-function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, { [key]: value});
+ };
+
+ function updateObjectWithKeyAndValue(object, key, value) {
+    return Object.assign({}, object, { [key]: value });
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
-  return object;
+ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+    object[key] = value;
+    return object;
 }
 
-function deleteFromObjectByKey(object, key) {
-  var newObj = object.assign({}, object);
-  delete newObj[key];
-  return newObj;
+ function deleteFromObjectByKey(object, key) {
+    var newObj = Object.assign({}, object);
+    delete newObj[key];
+    return newObj;
 }
 
-function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object[key];
-  return object;
+ function destructivelyDeleteFromObjectByKey(object, key) {
+    delete object[key];
+    return object;
 }
